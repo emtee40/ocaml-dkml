29fcb9ecb129009736a5c3c0188cc7bf1549691c
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2024-04-15 17:45:49 -0700

    Use Visual Studio discovered by setup-dkml

--- pkg/bump/clear-compiler-env.in.sh
+++ pkg/bump/clear-compiler-env.in.sh
@@ -10,3 +10,17 @@ unset DiskuvOCamlBinaryPaths
 unset DiskuvOCamlDeploymentId
 unset DiskuvOCamlVersion
 unset DiskuvOCamlMSYS2Dir
+
+# Use Visual Studio discovered by setup-dkml.
+case "$DKML_HOST_ABI" in
+    windows_*)
+        # shellcheck disable=SC1091
+        . "$BINARY_DIR/.ci/sd4/vsenv.sh"
+        export DKML_COMPILE_SPEC="1"
+        export DKML_COMPILE_TYPE="VS"
+        export DKML_COMPILE_VS_DIR="$VS_DIR"
+        export DKML_COMPILE_VS_VCVARSVER="$VS_VCVARSVER"
+        export DKML_COMPILE_VS_WINSDKVER="$VS_WINSDKVER"
+        export DKML_COMPILE_VS_MSVSPREFERENCE="$VS_MSVSPREFERENCE"
+        export DKML_COMPILE_VS_CMAKEGENERATOR="$VS_CMAKEGENERATOR"
+esac
