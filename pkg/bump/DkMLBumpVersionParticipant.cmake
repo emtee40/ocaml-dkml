88a4b39dd90f488fb2a0f01f4857bd9ea2eef2c4
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2024-05-28 04:33:15 -0700

    Bump DEFAULT_OCAML_OPAM_REPOSITORY_TAG

--- pkg/bump/DkMLBumpVersionParticipant.cmake
+++ pkg/bump/DkMLBumpVersionParticipant.cmake
@@ -237,6 +237,7 @@ endfunction()
 
 # ("DKML_VERSION", "1.1.0-prerel15"); -> ("DKML_VERSION", "1.2.1-3")
 # ("DEFAULT_DKML_COMPILER", "1.1.0-prerel15"); -> ("DEFAULT_DKML_COMPILER", "1.2.1-3");
+# ("DEFAULT_DISKUV_OPAM_REPOSITORY_TAG", "6c3f73f42890cc19f81eb1dec8023c2cd7b8b5cd"); -> ("DEFAULT_DISKUV_OPAM_REPOSITORY_TAG", "6c3f73f42890cc19f81eb1dec8023c2cd7b8b5cd")
 # ("DEFAULT_DISKUV_OPAM_REPOSITORY_TAG", "1.1.0-prerel15"); -> ("DEFAULT_DISKUV_OPAM_REPOSITORY_TAG", "1.2.1-3")
 # ("BOOTSTRAP_OPAM_VERSION", "..."); -> ("BOOTSTRAP_OPAM_VERSION", "2.2.0-alpha-20221228")
 #
@@ -256,6 +257,10 @@ function(DkMLBumpVersionParticipant_ModelReplace REL_FILENAME)
     string(REGEX REPLACE
         "\"DEFAULT_DKML_COMPILER\", \"${regex_DKML_VERSION_SEMVER}\""
         "\"DEFAULT_DKML_COMPILER\", \"${DKML_VERSION_SEMVER_NEW}\""
+        contents_NEW "${contents_NEW}")
+    string(REGEX REPLACE
+        "\"DEFAULT_OCAML_OPAM_REPOSITORY_TAG\", \"[0-9a-f]*\""
+        "\"DEFAULT_OCAML_OPAM_REPOSITORY_TAG\", \"${OCAML_OPAM_REPOSITORY_GITREF}\""
         contents_NEW "${contents_NEW}")
     string(REGEX REPLACE
         "\"DEFAULT_DISKUV_OPAM_REPOSITORY_TAG\", \"${regex_DKML_VERSION_SEMVER}\""
