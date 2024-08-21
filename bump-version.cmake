5d95633a39b803c69f13e6a8255a4050f0b4a768
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2024-08-20 17:50:12 -0700

    Squashed 'vendor/dkml-compiler/' changes from e077b9342..39458084b
    39458084b Use dkml-host-abi package
    61fbd641b Merge commit 'd6e0ecf645f8a44a0b348ada6d509679d3ae0893' into next
    292aca80b Change path for DkMLBumpVersionParticipant comment
    
    git-subtree-dir: vendor/dkml-compiler
    git-subtree-split: 39458084b1152b53fad809536023f45e5d99d8f4

--- bump-version.cmake
+++ bump-version.cmake
@@ -1,5 +1,5 @@
 # Test with:
-# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-compiler -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.2 -D DKML_BUMP_VERSION_PARTICIPANT_MODULE=../../../pkg/bump/DkMLBumpVersionParticipant.cmake -P bump-version.cmake
+# cmake --log-context -D DRYRUN=1 -D CMAKE_MESSAGE_CONTEXT=dkml-compiler -D "regex_DKML_VERSION_SEMVER=1[.]2[.]1-[0-9]+" -D "regex_DKML_VERSION_OPAMVER=1[.]2[.]1[~]prerel[0-9]+" -D DKML_VERSION_SEMVER_NEW=1.2.1-3 -D DKML_VERSION_OPAMVER_NEW=1.2.1~prerel3 -D GIT_EXECUTABLE=git -D DKML_RELEASE_OCAML_VERSION=4.14.2 -D DKML_BUMP_VERSION_PARTICIPANT_MODULE=../../pkg/bump/DkMLBumpVersionParticipant.cmake -P bump-version.cmake
 
 if(NOT DKML_BUMP_VERSION_PARTICIPANT_MODULE)
     message(FATAL_ERROR "Missing -D DKML_BUMP_VERSION_PARTICIPANT_MODULE=.../DkMLBumpVersionParticipant.cmake")
