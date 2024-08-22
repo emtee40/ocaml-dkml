38def7e272856ab3bdf6d11b27393a587a04a9e3
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2024-08-21 22:44:24 -0700

    Squashed 'vendor/dkml-runtime-common/' changes from 9cd5751b3..78edd9887
    78edd9887 Remove dkml-runtime-common-native
    3d0c5b4a4 Remove dune from dkml-runtime-common
    9fa502eb8 bugfix: Remove CR from CRLF when installing scripts
    
    git-subtree-dir: vendor/dkml-runtime-common
    git-subtree-split: 78edd988772b95e952e658959d6772e2fe624624

--- install.sh
+++ install.sh
@@ -12,5 +12,15 @@ install -d "$targetdir/macos" "$targetdir/unix"
 
 install META "$targetdir/"
 install template.dkmlroot "$targetdir/"
-install macos/brewbundle.sh "$targetdir/macos/"
-install unix/_common_tool.sh unix/_within_dev.sh unix/crossplatform-functions.sh "$targetdir/unix/"
+
+# Copy scripts. No CRLF allowed.
+#
+#   shellcheck disable=SC2043
+for i in brewbundle.sh; do
+    tr -d '\r' < macos/$i > "$targetdir/macos/$i"
+    chmod +x "$targetdir/macos/$i"
+done
+for i in _common_tool.sh _within_dev.sh crossplatform-functions.sh; do
+    tr -d '\r' < unix/$i > "$targetdir/unix/$i"
+    chmod +x "$targetdir/unix/$i"
+done
